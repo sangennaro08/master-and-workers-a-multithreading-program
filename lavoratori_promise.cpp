a8844baa3c5f98cd51b956a37016cd3d91290855
sangennaro08 <136181657+sangennaro08@users.noreply.github.com>
2025-10-22 19:53:42 +0200

    lavoratori_promise.cpp
    migliorie nel codice come:
    1)utilizzo di 1 condition variable per evitare busy waiting nel main
    
    2)utlizzo di smart pointer per evitare leak di memoria 
    
    3)utlizzo di vector.reserve();
    
    l'aggiunta più importante in quanto permette di evitare praticamente sempre il reference dangling.
    infatti nel mentre le task venivano inserite, i thread venivano subito inizializzate e iniziavano i task appena inseriti.
    Questo è grave in quanto mentre le task vengono inserite i thread potrebbero riferirsi a una cella di memoria non valida facendo avvenire il reference dangling.

--- lavoratori_promise.cpp
+++ lavoratori_promise.cpp
@@ -7,11 +7,15 @@
 #include <cstdlib>
 #include <random>
 #include <queue>
+#include <memory>
+#include <condition_variable>
 
 using namespace std;
 
 mutex mut;
 mutex dip;
+mutex m;
+condition_variable cv;
 
 int lavori=20;
 int dipendenti=5;
@@ -49,12 +53,12 @@ class worker{
     public:
 
     int ID_worker=0;
-    task* t;
+    shared_ptr<task> t;
     thread th;
     promise<double> final;
     mt19937 generatore;
 
-    worker(int ID,queue <task*>& task_generated):
+    worker(int ID,queue <shared_ptr<task>>& task_generated):
     generatore(chrono::system_clock::now().time_since_epoch().count() + ID)
     {
 
@@ -72,20 +76,16 @@ class worker{
 
     }
 
-    
-    ~worker(){delete t;}
-
-    void start_work(queue <task*>* task_generated){
+    void start_work(queue <shared_ptr<task>>* task_generated){
 
         while(true){
 
-            {
-                
+            {               
                 lock_guard<mutex> lock(mut);
                 cout<<"il lavoratore "<<ID_worker<<" sta iniziando la task di tipo "<<t->tipo<<" che dura "
                 <<t->tempo_necessario<<" secondi\n\n";
             
-                thread ta(&worker::adopera,this,ref(t->adding));
+                thread ta(&worker::adopera,this,&t->adding);
                 ta.join();
 
             }
@@ -99,7 +99,7 @@ class worker{
                 cout<<"il thread con ID "<<ID_worker<<" ha finito il lavoro "<<t->tipo<<",esso era una somma e gli è durato "
                 <<t->tempo_necessario<<" secondi"<<" i numeri erano "<<t->num1<<" e "<<t->num2<<" abbiamo ottenuto "<<f<<"\n\n";
 
-                if(!get_new_work(&task_generated))return;
+                if(!get_new_work(*(&task_generated)))return;
 
             }   
             
@@ -109,24 +109,27 @@ class worker{
 
 
     //funzione che permette a un worker di ottenere un lavoro disponibile al momento
-    bool get_new_work(queue <task*>** task_generated){
+    bool get_new_work(queue <shared_ptr<task>>* task_generated){
 
         {
             lock_guard<mutex> lock(dip);
 
-            if((**task_generated).size()!=0){
+            if((*task_generated).size()!=0){
 
-                t=(**task_generated).front();
-                cout<<"il lavoratore "<<ID_worker<<" ha deciso di prendere il lavoro di tipo "<<(**task_generated).front()->tipo<<"\n\n";
-                (**task_generated).pop();
+                t=(*task_generated).front();
+                cout<<"il lavoratore "<<ID_worker<<" ha deciso di prendere il lavoro di tipo "<<(*task_generated).front()->tipo<<"\n\n";
+                (*task_generated).pop();
 
                 return true;
 
             }
 
             dipendenti--;
-            return false;
 
+            if(dipendenti==0){cv.notify_one();}
+                       
+            return false;
+            
         }
 
     }   
@@ -143,7 +146,7 @@ class worker{
     }
 
     //somma dei numeri
-    void adopera(promise <double>& adding){
+    void adopera(promise <double>* adding){
         
         {
             lock_guard<mutex> lock(dip);
@@ -153,7 +156,7 @@ class worker{
 
             t->num1 = distribution(generatore);
             t->num2 = distribution(generatore);
-            adding.set_value(t->num1+t->num2); 
+            adding->set_value(t->num1+t->num2); 
         }
 
     }
@@ -164,16 +167,17 @@ class master{
 
     public:
 
-    vector <worker*> workers;
-    queue <task*>   task_generated;
+    vector <worker> workers;
+    queue <shared_ptr<task>> task_generated;
 
     void create_workers(){
 
+        workers.reserve(dipendenti);//fondamentale in quanto senza di esso il vettore durante i push_back cambierebbe posizioni
+        //nell'heap nel bel mezzo dei thread ceh stanno prendendo le task e pescandone altri
+
         for(int i=0;i<dipendenti;i++){
 
-            promise<double> obtain_ris;
-
-            workers.push_back(new worker(i,task_generated));
+            workers.emplace_back(i,task_generated);
 
         }
 
@@ -183,30 +187,18 @@ class master{
 
         for(int i=0;i<lavori;i++){
 
-            task_generated.push(new task((rand()%10)+1,type_task[i]));
+            task_generated.push(make_shared<task>((rand()%10)+1,type_task[i]));
             
 
         }
 
     }
 
-    void delete_workers(){
+   void finish_threads(){
 
-        for(auto w : workers){
+        for(auto& w : workers){
 
-            w->control_thread();
-            delete w;
-        }
-
-    }
-
-    void delete_tasks(){
-
-        while(!task_generated.empty()){
-
-            delete task_generated.front();        
-            task_generated.pop();
-
+            w.control_thread();
         }
 
     }
@@ -223,17 +215,41 @@ int main(){
     M.create_tasks();
     M.create_workers();
 
-    while(dipendenti!=0){
+    //while(dipendenti!=0){
 
-
-    
+    {
+        unique_lock<mutex> lock(m);
+        cv.wait(lock,[]{return (dipendenti==0)?true :false;});
     }
+    
+    
+    //}
 
     cout<<"tutti i dipendenti hanno finito di lavorare\n";
-    
-    //libero memoria allocata
-    M.delete_workers();
-    M.delete_tasks();
+    M.finish_threads();
 
     return 0;
 }
+
+/*
+problemi affrontati per la miglioria del codice...
+
+1)movimento del vettore nell'heap costante durante la selezione delle task per gli worker
+
+per risolvere il problema ho utlizzato una funzione dei vettori chiamata 
+
+nome.reserve(n);
+
+indica al vettore di prendere in anticipo n celle di memoria per la allocazione di memoria questo permette al vettore di prendere uno spazio
+totale gia dall'inizio senza che esso debba muoversi facendo dei push_back
+
+2)utilizzo di smart pointers
+
+utilizzati per evitare l'utlizzo di distruttori e togliere il pensiero sulla deallocazione di memoria(il controllo del funzionamento dei 
+thread nche se la cella non è piu utlizzata dal queue è ancora con un controllo manuale)
+
+è stato modificato anche la funzione che disabilita i thread con l'aggiunta di un & nelle parentesi rotonde,infatti senza di esso
+non troverebbe gli effettivi oggetti collocati in memoria in quanto eliminati da soli a causa degli smart pointers.
+
+
+*/
